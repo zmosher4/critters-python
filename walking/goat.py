fb2894ade996769a3606d11cffdc39b23a5bc7a7
Zach Mosher <zmosher4@gmail.com>
2024-08-14 11:06:18 -0500

    completed chapter 5

--- walking/goat.py
+++ walking/goat.py
@@ -3,8 +3,9 @@ from datetime import date
 
 class Goat:
 
-    def __init__(self, name, species):
+    def __init__(self, name, species, shift):
         self.name = name
         self.species = species
         self.date_added = date.today
         self.walking = True
+        self.shift = shift
